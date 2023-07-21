c86279e9ef7f8f0234b40bc6c26da5663ba076ea
samyag.shah <samyag.shah@agileinfoways.com>
2023-07-21 17:59:27 +0530

    deploy.sh is updated

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-export NVM_DIR=$HOME/.nvm;
-. $NVM_DIR/nvm.sh;
-
-cd /home/ubuntu/deploy_nextjs_app
+cd /home/ubuntu/nextjs
 git pull origin master
 npm install &&
 npm run build &&
-pm2 restart next_hello_world_app
+pm2 restart nextjs
